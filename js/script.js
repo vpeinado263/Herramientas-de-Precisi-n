14299c964083b61d61101594c539da54f70aa9dc
Victor Peinado <vpeinado263@gmail.com>
2024-10-20 12:52:19 -0300

    se crean las primeras variables de seleccion de elementos del DOM

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*****Contador*****/
+const contadorElement = document.getElementById("contador");
+const aumentarButton = document.getElementById("aumentar");
+const disminuirButton = document.getElementById("disminuir");
+let contador = 0;
+function actualizarContador(){
+    
+}
