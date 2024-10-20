f5579d627869cbc8716f4c7e3d65d6822a08477f
Victor Peinado <vpeinado263@gmail.com>
2024-10-20 15:16:51 -0300

    Se crea la lógica del cronometro

--- js/script.js
+++ js/script.js
@@ -24,23 +24,36 @@ actualizarContador();
 //Cronómetro
 let minutos = 0;
 let segundos = 0;
-let milisegundo = 0;
+let milisegundos = 0;
+let intervalo;
 
 function iniciarCronometro() {
-
+    detenerCronometro();
+    intervalo = setInterval(actualizarCronometro, 10);
 }
 function detenerCronometro() {
-
+    clearInterval(intervalo);
 }
 function resetearCronometro() {
-
+    detenerCronometro();
+    minutos = 0;
+    segundos = 0;
+    milisegundos = 0;
+    actualizarDisplay();
 }
 function actualizarCronometro() {
-
+    milisegundos += 10;
+    if (milisegundos === 1000) {
+        milisegundos = 0;
+        minutos++;
+    }
+    actualizarDisplay();
 }
 function actualizarDisplay() {
-
+    document.getElementById('minutos').innerText = formatearTiempo(minutos);
+    document.getElementById('segundos').innerText = formatearTiempo(segundos);
+    document.getElementById('milisegundos').innerText = formatearTiempo(milisegundos)
 }
 function formatearTiempo() {
-    
+    return tiempo < 10 ? '0' + tiempo : tiempo;
 }
