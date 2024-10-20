ccb2a0009ed21c02c1eee9d57b5b5abaf639af49
Victor Peinado <vpeinado263@gmail.com>
2024-10-20 13:21:26 -0300

    Creamos la logica del contador

--- js/script.js
+++ js/script.js
@@ -4,5 +4,20 @@ const aumentarButton = document.getElementById("aumentar");
 const disminuirButton = document.getElementById("disminuir");
 let contador = 0;
 function actualizarContador(){
-    
+    contadorElement.textContent = contador;
+    contadorElement.classList.remove("animated");
+    setTimeout(() => {
+        contadorElement.classList.remove("animated");
+    }, 300);
 }
+aumentarButton.addEventListener("click", function() {
+    contador++;
+    actualizarContador();
+});
+disminuirButton.addEventListener("click", function() {
+    if (contador > 0) {
+        contador--;
+        actualizarContador();
+    }
+});
+actualizarContador();
